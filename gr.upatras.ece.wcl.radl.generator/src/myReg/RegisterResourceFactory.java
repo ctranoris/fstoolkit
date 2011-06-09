c4cb719955f37fb3537e08979a42c824168a5eb1
tranoris <tranoris@88d6be61-b049-4d1a-ac2d-38c3f0f8cafa>
2011-06-09 12:58:50 +0000

    git-svn-id: svn://nam.ece.upatras.gr/nam/software/fstoolkit@67 88d6be61-b049-4d1a-ac2d-38c3f0f8cafa

---
+++ gr.upatras.ece.wcl.radl.generator/src/myReg/RegisterResourceFactory.java
@@ -0,0 +1,30 @@
+package myReg;
+
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.mwe.core.WorkflowContext;
+import org.eclipse.emf.mwe.core.issues.Issues;
+import org.eclipse.emf.mwe.core.lib.AbstractWorkflowComponent;
+import org.eclipse.emf.mwe.core.monitor.ProgressMonitor;
+import org.panlab.officedl.OfficeDLStandaloneSetup;
+import org.panlab.officedl.OfficeDLStandaloneSetupGenerated;
+
+
+public class RegisterResourceFactory extends AbstractWorkflowComponent {
+
+	@Override
+	public void checkConfiguration(Issues issues) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void invokeInternal(WorkflowContext ctx, ProgressMonitor monitor,
+			Issues issues) {
+
+		System.out.println("---------------> invokeInternal new OfficeDLStandaloneSetup().createInjectorAndDoEMFRegistration();");
+		new OfficeDLStandaloneSetup().createInjectorAndDoEMFRegistration();
+
+	}
+
+}
