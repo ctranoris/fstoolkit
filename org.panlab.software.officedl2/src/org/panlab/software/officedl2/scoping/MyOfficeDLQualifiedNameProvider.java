d7d9a2f9117fd822b67f6a60c82318a7e783ba12
tranoris <tranoris@88d6be61-b049-4d1a-ac2d-38c3f0f8cafa>
2011-12-26 21:34:25 +0000

    CLOSED - # 13: Change fully qualified name for service and resource names http://nam.ece.upatras.gr/fstoolkit/trac/ticket/13
    git-svn-id: svn://nam.ece.upatras.gr/nam/software/fstoolkit@161 88d6be61-b049-4d1a-ac2d-38c3f0f8cafa

--- org.panlab.software.officedl2/src/org/panlab/software/officedl2/scoping/MyOfficeDLQualifiedNameProvider.java
+++ org.panlab.software.officedl2/src/org/panlab/software/officedl2/scoping/MyOfficeDLQualifiedNameProvider.java
@@ -1,6 +1,11 @@
 package org.panlab.software.officedl2.scoping;
 
 import org.eclipse.xtext.naming.DefaultDeclarativeQualifiedNameProvider;
+import org.eclipse.xtext.naming.QualifiedName;
+
+import FederationOffice.Office;
+import FederationOffice.federationscenarios.ServiceRequest;
+import FederationOffice.federationscenarios.ServiceSettingInstance;
 
 public class MyOfficeDLQualifiedNameProvider extends
 		DefaultDeclarativeQualifiedNameProvider {
@@ -15,4 +20,28 @@ public class MyOfficeDLQualifiedNameProvider extends
 //	public String qualifiedName(FederationOffice.resources.OfferedResource obj) {
 //		return obj.getName();
 //	}
+	
+//	public String qualifiedName(FederationOffice.resources.OfferedResource obj) {
+//		return ( obj.getName() ); 
+//	}
+	
+	
+//	Qualified Name should be the same in the proposal Name
+	
+	public QualifiedName qualifiedName( FederationOffice.resources.OfferedResource obj) {
+		//System.out.println(">>>qualifiedName ServiceSettingInstance>>>="+ obj.toString() );
+		String n = obj.getName() ;//obj.getFullQualifiedName();
+		QualifiedName qualifiedNameFromConverter = getConverter().toQualifiedName(n);
+		return qualifiedNameFromConverter;
+		
+	}
+	
+
+	public QualifiedName qualifiedName( FederationOffice.services.OfferedService  obj) {
+		//System.out.println(">>>qualifiedName ServiceSettingInstance>>>="+ obj.toString() );
+		String n =  obj.getName()+"."+((Office)obj.eContainer()).getName();
+		QualifiedName qualifiedNameFromConverter = getConverter().toQualifiedName(n);
+		return qualifiedNameFromConverter;
+		
+	}
 }
